f4fd185ec55394d1a4cec15d169f7a8e87af1b35
Nick Macon <nickmacon@gmail.com>
2019-10-29 21:37:19 -0500

    10.29 push

--- app.js
+++ app.js
@@ -1,52 +1,95 @@
-// Example queryURL for Giphy API
-$("#find-animal").on("click", function(event) {
+var movies = [];
 
-    // Preventing the submit button from trying to submit the form
-    // We're optionally using a form so the user may hit Enter to search instead of clicking the button
-    event.preventDefault();
+// Function for displaying movie data
+function renderButtons() {
 
-    // Here we grab the text from the input box
-    var animal = $("#animal-input").val();
+  // Deleting the movies prior to adding new movies
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
+  
+  // Looping through the array of movies
+  for (var i = 0; i < movies.length; i++) {
+  
+    // Then dynamicaly generating buttons for each movie in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of movie-btn to our button
+    a.addClass("movie-btn");
+    // Adding a data-attribute
+    a.attr("data-name", movies[i]);
+    // Providing the initial button text
+    a.text(movies[i]);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
+  }
+  
+  // This function handles events where a movie button is clicked
+  $("#add-movie").on("click", function(event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var movie = $("#movie-input").val().trim();
+  
+  // Adding movie from the textbox to our array
+  movies.push(movie);
+  
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
+    });
+  
+  
+  // Adding a click event listener to all elements with a class of "movie-btn"
+  $(document).on("click", ".movie-btn", displayMovieInfo);
+  
+  // // Calling the renderButtons function to display the intial buttons
+  renderButtons();
 
-// $("button").on("click", function() {
-    // var animal = $(this).attr("data-animal");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=Tj0oPNZniOJy89T0vNhvVfFjmBFz1F3S&limit=10";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-    .then(function(response) {
-        // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
-        // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
+// Function for dumping the JSON content for each button into the div
+function displayMovieInfo() {
 
-        console.log(response);
+  var movie = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=Tj0oPNZniOJy89T0vNhvVfFjmBFz1F3S&limit=10";
+// Function for displaying movie data
+// Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  }).then(function(response) {
 
-        // Step 2: since the image information is inside of the data key,
-        // make a variable named results and set it equal to response.data
+    console.log(response);
 
-        // =============== put step 2 in between these dashes ==================
-        var results = response.data;
-        // ========================
+    // Step 2: since the image information is inside of the data key,
+    // make a variable named results and set it equal to response.data
+    
+    // =============== put step 2 in between these dashes ==================
+    var results = response.data;
+    // ========================
+    
+    for (var i = 0; i < results.length; i++) {
+    
+    // Step 3: uncomment the for loop above and the closing curly bracket below.
+    var animalDiv = $("<div>");
+    var p = $("<p>");
+    p.text("Rating: " + results[i].rating);
+    console.log(results[i].rating);
+    var animalImage = $("<img>");
+    animalImage.attr("src", results[i].images.fixed_height.url);
+    animalDiv.append(p);
+    animalDiv.append(animalImage);
+    $("#movies-view").prepend(animalDiv);
+    };
+  });
+}
 
-        for (var i = 0; i < results.length; i++) {
 
-        // Step 3: uncomment the for loop above and the closing curly bracket below.
-        var animalDiv = $("<div>");
-        var p = $("<p>");
-        $("<p>").text("Rating: " + results[i].rating);
-        var animalImage = $("<img>");
-        animalImage.attr("src", results[i].images.fixed_height.url);
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
-        $("#gifs-appear-here").prepend(animalDiv);
 
-        // ============= put step 3 in between these dashes ======================
-        
-        // ==================================
-        }
+
+
+
+
+
 
         // $(".gif").on("click", function() {
         //     // STEP ONE: study the html above.
@@ -86,5 +129,5 @@ $("#find-animal").on("click", function(event) {
             // STEP FOUR: open the file in the browser and click on the images.
             // Then click again to pause.
         //   });
-      });
-    });
+    //   });
+// });
